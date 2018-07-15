45970a3fb9541962f52e622152077223dd07dc3d
Goncalo101 <goncalo.oli@outlook.com>
2018-07-15 21:22:24 +0100

    Updated bootloader to use the new print and print_hex functions

--- boot.asm
+++ boot.asm
@@ -1,5 +1,17 @@
-loop: ; infinite loop
-	jmp loop
+[org 0x7c00]
+
+mov si, msg1
+call print
+
+mov dx, 0xcafe
+call print_hex
+
+jmp $
+
+%include "boot_utils/print.asm"
+%include "boot_utils/print_hex.asm"
+
+msg1 db 'Hello World!', 13, 10, 0
 
 times 510 - ($-$$) db 0
 dw 0xaa55 ; magic number (BIOS will consider the device bootable if it finds
