bb164a7bd147b0d606ad360d97f5544a87f1e44f
Goncalo101 <goncalo.oli@outlook.com>
2018-07-26 13:02:52 +0100

    Added new types and updated drivers to use them

---
+++ cpu/types.h
@@ -0,0 +1,16 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+/* defines new types with no semantic meaning */
+typedef unsigned int u32;
+typedef int s32;
+typedef unsigned short u16;
+typedef short s16;
+typedef unsigned char u8;
+typedef char s8;
+
+// define functions to return the low or high 16 bits of an address
+#define low_16(address) (u16)((address) & 0xFFFF)
+#define high_16(address) (u16)(((address) >> 16) & 0xFFFF)
+
+#endif
--- drivers/ports.c
+++ drivers/ports.c
@@ -1,7 +1,9 @@
+#include "ports.h"
+
 // Read a byte from the specified port
-unsigned char port_byte_in(unsigned short port)
+u8 port_byte_in(u16 port)
 {
-  unsigned char result;
+  u8 result;
 
   /* "=a" (result)'; set '=' the C variable '(result)' to the value of register e'a'x
   *  '"d" (port)': map the C variable '(port)' into e'd'x register
@@ -15,21 +17,21 @@ unsigned char port_byte_in(unsigned short port)
 
 // when requesting data from a VGA register, the result is in a different port,
 // which requires us to use port_byte_in to get it
-void port_byte_out(unsigned short port, unsigned char data)
+void port_byte_out(u16 port, u8 data)
 {
     __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
 }
 
-unsigned short port_word_in(unsigned short port)
+u16 port_word_in(u16 port)
 {
-    unsigned short result;
+    u16 result;
 
     __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
 
     return result;
 }
 
-void port_word_out(unsigned short port, unsigned short data)
+void port_word_out(u16 port, u16 data)
 {
     __asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
 }
